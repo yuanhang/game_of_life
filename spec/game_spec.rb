bb99e086c4133128739451673d7c122715b5ee9d
yuanhang <yuanhang.guo@gmail.com>
2014-02-25 14:57:10 +0800

    refactoring

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,18 +1,24 @@
 require 'spec_helper'
 
-def assert_grid init_grid
+def assert_grid init_grid, expected_grid
   game = Game.new init_grid
-  game.tick.should == [[0]]
+  game.tick.should == expected_grid
 end
 
 describe Game do 
   context "only one cell" do 
     it "dead cell will still be dead" do 
-      assert_grid [[0]]
+      assert_grid [[0]], [[0]]
     end
 
     it "live cell will be dead" do 
-      assert_grid [[1]]
+      assert_grid [[1]], [[0]]
     end
   end
+
+  #context "two cells" do
+    #it "dead cell will still be dead" do
+      #assert_grid [[0], [0]]
+    #end
+  #end
 end
