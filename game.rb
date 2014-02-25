c92601c12302fe2a0a3ab33b5022ee7eec281fe0
yuanhang <yuanhang.guo@gmail.com>
2014-02-25 14:38:11 +0800

    simplest case

--- game.rb
+++ game.rb
@@ -1,3 +1,8 @@
 class Game
+  def initialize(init_grid)
+  end
 
+  def tick
+    [[0]]
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,4 +1,10 @@
-require_relative 'spec_helper'
+require 'spec_helper'
 
 describe Game do 
+  context "only one cell" do 
+    it "dead cell will still be dead" do 
+      game = Game.new [[0]]
+      game.tick.should == [[0]]
+    end
+  end
 end
